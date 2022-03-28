fd84859bf2ea1ce2d90d67323f3bca1be08d825b
he55 <40764272+he55@users.noreply.github.com>
2022-03-28 18:44:16 +0800

    add code

--- ProjectNative/ProjectNative.cpp
+++ ProjectNative/ProjectNative.cpp
@@ -4,18 +4,135 @@
 #include "framework.h"
 #include "ProjectNative.h"
 
+#include <stdlib.h>
+#include <assert.h>
+#include <Windows.h>
 
-// This is an example of an exported variable
-PROJECTNATIVE_API int nProjectNative=0;
 
-// This is an example of an exported function.
-PROJECTNATIVE_API int fnProjectNative(void)
+MyStruct* rkrs_open_file(const char* pszPathName)
 {
-    return 0;
+	HANDLE hFile = CreateFile(pszPathName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+		return NULL;
+
+	DWORD dwFileSize = GetFileSize(hFile, NULL);
+	HANDLE hFileMap = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, dwFileSize, NULL);
+	if (hFileMap == NULL) {
+		CloseHandle(hFile);
+		return NULL;
+	}
+
+	void* pvFile = MapViewOfFile(hFileMap, FILE_MAP_READ, 0, 0, 0);
+	if (pvFile == NULL) {
+		CloseHandle(hFileMap);
+		CloseHandle(hFile);
+		return NULL;
+	}
+
+	MyStruct* mys = (MyStruct*)malloc(sizeof(MyStruct));
+	assert(mys != NULL);
+
+	*mys = { pvFile,hFileMap,hFile };
+	return mys;
 }
 
-// This is the constructor of a class that has been exported.
-CProjectNative::CProjectNative()
+
+void rkrs_close_file(MyStruct* mys)
 {
-    return;
+	assert(mys != NULL);
+
+	UnmapViewOfFile(mys->pvFile);
+	CloseHandle(mys->hFileMap);
+	CloseHandle(mys->hFile);
+
+	free(mys->mys2.ppbidd);
+	free(mys);
+}
+
+
+void rkrs_parse(MyStruct* mys, MyStruct2* mys2)
+{
+	assert(mys != NULL);
+	assert(mys2 != NULL);
+
+	void* pv = mys->pvFile;
+	HEADER_H* h = (HEADER_H*)pv;
+	RKRS_H* rkrs = (RKRS_H*)((char*)pv + 0x30);
+	BID_H* bid = (BID_H*)((char*)pv + rkrs->offset);
+
+	BIDD_H** ppbidd = (BIDD_H**)malloc(sizeof(void*) * rkrs->count);
+	assert(ppbidd != NULL);
+
+	for (size_t i = 0; i < rkrs->count; i++)
+		ppbidd[i] = (BIDD_H*)((char*)pv + bid[i].offset);
+
+	*mys2 = mys->mys2 = { h,rkrs,bid,ppbidd };
+}
+
+
+void* rkrs_read_image_data(MyStruct* mys, int idx)
+{
+	assert(mys != NULL);
+
+	void* pv = mys->pvFile;
+	RKRS_H* rkrs = (RKRS_H*)((char*)pv + 0x30);
+	assert(idx < rkrs->count);
+
+	BID_H* bid = (BID_H*)((char*)pv + rkrs->offset);
+	BIDD_H* bidd = (BIDD_H*)((char*)pv + bid[idx].offset);
+
+	BIDD_H _bidd = *bidd;
+	int* data = (int*)bidd->data;
+	int* bitmap = (int*)malloc(sizeof(int) * _bidd.width * _bidd.height);
+	assert(bitmap != NULL);
+
+	int* _bitmap = bitmap;
+
+	if (_bidd.d4 == 1)
+	{
+		int v = 0;
+		while (v = *(data++))
+		{
+			int val = v & 0x00ffffff;
+			if (val != 0x00ff00ff)
+				val |= 0xff000000;
+
+			int len = v >> 24 & 0xff;
+			if (len == 0xff)
+				len += *(data++);
+
+			for (int i = 0; i < len; i++)
+				*(_bitmap++) = val;
+		}
+	}
+	else if (_bidd.d4 == 0)
+	{
+		int pix = _bidd.width * _bidd.height;
+		int curpix = 0;
+		while (curpix++ < pix) {
+			int val = *(data++);
+			if (val != 0x00ff00ff)
+				val |= 0xff000000;
+
+			*(_bitmap++) = val;
+		}
+	}
+	else if (_bidd.d4 == 5)
+	{
+		int alpha = 0;
+		if (_bidd.d5 == 3)
+			alpha = 0xff000000;
+
+		int pix = _bidd.width * _bidd.height;
+		int curpix = 0;
+		while (curpix++ < pix)
+			*(_bitmap++) = *(data++) | alpha;
+	}
+	return (void*)bitmap;
+}
+
+
+void rkrs_free_image_data(void * img)
+{
+	free(img);
 }
