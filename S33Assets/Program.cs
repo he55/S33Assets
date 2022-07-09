1a91618d41f7581c21df0368fcf1b986ace4bf3a
he55 <40764272+he55@users.noreply.github.com>
2022-07-09 12:42:01 +0800

    Update Program.cs

--- S33Assets/Program.cs
+++ S33Assets/Program.cs
@@ -1,17 +1,26 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace S33Assets
 {
-    static class Program
+    internal static class Program
     {
+        [DllImport("User32.dll", SetLastError = false, ExactSpelling = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetProcessDPIAware();
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+#if NETCOREAPP3_0_OR_GREATER
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
+#else
+            SetProcessDPIAware();
+#endif
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
